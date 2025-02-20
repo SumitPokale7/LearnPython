ba06fdd138ab3d4551dc91f662316a29306d9036
SumitPokale7 <pokalesumit5@gmail.com>
2025-02-20 20:57:33 +0530

    Added twenty-first project (Guess States)

---
+++ guess_states21/main.py
@@ -0,0 +1,35 @@
+import turtle
+import pandas
+
+
+screen = turtle.Screen()
+screen.title("India State Game")
+image = r"E:\\College\\Python\\squirrel-count21\\Blank_state.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+
+guessed_states = []
+while len(guessed_states) < 28:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
+
+    data = pandas.read_csv(r'E:\\College\\Python\\squirrel-count21\\28_states.csv')
+
+    all_states = data.State.to_list()
+
+    if answer_state == "Exit":
+        missing_state = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_state.append(state)
+        missing_data = pandas.DataFrame(missing_state)
+        missing_data.to_csv("States_Not_Guessed.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.State == answer_state]
+        t.goto(state_data.X.item(), state_data.Y.item())
+        t.write(answer_state)
