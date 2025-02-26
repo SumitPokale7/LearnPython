e82984f2620241ae7aead35e159304ee327644fe
Sumit Pokale <pokalesumit4@gmail.com>
2025-02-26 12:12:40 +0530

    Added Exception handling for Password manager & Added Search button

--- password-manager25/main.py
+++ password-manager25/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 # ---------------------------- CONSTANTS ------------------------------- #
 WHITE = "#FFFFFF"
@@ -30,17 +31,50 @@ def generate_password():
 def save_password():
     website = website_entry.get()
     email = email_entry.get()
-    password  = password_entry.get()
+    password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
-    if len(website) == 0:
-        messagebox.showwarning(title="Oops!", message="Please make sure you haven't left any fields empty!")
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok save?")
-        if is_ok:
-            with open("Saved_Password.txt", "a") as w:
-                w.write(f"Website: {website} | Email: {email} | Password: {password} |\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        try:
+            with open("Saved_Passwords.json", "r") as data_file:
+                #Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("Saved_Passwords.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            #Updating old data with new data
+            data.update(new_data)
+
+            with open("Saved_Passwords.json", "w") as data_file:
+                #Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_passwords():
+    website = website_entry.get()
+    try:
+        with open("Saved_Passwords.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Saved Password file found!")
+    else:
+        if website in data:
+            saved = data[website]
+            messagebox.showinfo(title=website, message=f"Email: {saved['email']} \nPassword: {saved['password']}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -55,6 +89,7 @@ canvas.create_image(100, 100, image=logo_png)
 canvas.grid(column=1, row=0)
 
 
+# Labels
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
 
@@ -62,13 +97,14 @@ website_label.grid(column=0, row=1)
 email_label = Label(text="Email/Username:")
 email_label.grid(column=0, row=2)
 
+
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
 
 # Value Entries 
-website_entry = Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(column=1, row=1)
 website_entry.focus()
 
 
@@ -82,6 +118,10 @@ password_entry.grid(column=1, row=3)
 
 
 # Buttons
+search_button = Button(text="Search", width=13, command=find_passwords)
+search_button.grid(column=2, row=1)
+
+
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(column=2, row=3)
 
