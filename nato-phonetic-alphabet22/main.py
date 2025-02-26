2f61f2632927f02cf8886c55412ca79e3a3ee7a7
Sumit Pokale <pokalesumit4@gmail.com>
2025-02-26 11:18:43 +0530

    Added Exception handling for Nato phonetic alphabet project

--- nato-phonetic-alphabet22/main.py
+++ nato-phonetic-alphabet22/main.py
@@ -1,9 +1,19 @@
 import pandas
 
-data = pandas.read_csv(r'E:\\College\\Python\\22\\nato_phonetic_alphabet.csv')
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 new_dict = {row.letter: row.code for _, row in data.iterrows()}
-word = input("Enter a word: ").upper()
-output_list = [new_dict[letter] for letter in word]
 
-print(output_list)
+
+def get_input():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [new_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        get_input()
+    else:
+        return output_list
+
+
+get_input()
