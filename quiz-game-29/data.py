f6c30282a4148fd4814d1ad8f67375090bd826ad
SumitPokale7 <pokalesumit5@gmail.com>
2025-03-03 20:20:23 +0530

    Added twenty-nine project (Quiz Game with GUI)

--- quiz-game-29/data.py
+++ quiz-game-29/data.py
@@ -1,14 +1,12 @@
-import requests # type: ignore
+import requests
 
 
-parameters = {
-    "amount": 10,
-    "type": "boolean"
-}
+parameters = {"amount": 10, "type": "boolean"}
 
 
 response = requests.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
-data = response.json() 
 
-question_data = data['results']
+data = response.json()
+
+question_data = data["results"]
